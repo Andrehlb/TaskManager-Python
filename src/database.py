f4eb82787743246b61c54c855ff33cbb31821b76
Andrehlb <andrehlb@me.com>
2023-12-17 13:38:03 -0300

    Implementing the SQLAlchemy configuration, including the creation of the engine, session, and database initialization.

--- src/database.py
+++ src/database.py
@@ -3,3 +3,13 @@ from sqlalchemy.orm import scoped_Session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Aqui é o caminho/path do banco de dados
+engine = create_engine('sqlite:///database.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    # Aqui: importação dos modelos para que sejam registrados no metadados do SQLAlchemy
+    from models import Task # Substituir 'Task' pelo nome do modelo
+    Base.metadata.create_all(bind=engine)
