da22b17e73a6c3c17410d21553278b6344443182
Andrehlb <andrehlb@me.com>
2023-12-17 12:30:29 -0300

    Start SQLAlchemy configuration, including imports and preparation for defining the database path.

--- src/database.py
+++ src/database.py
@@ -1,2 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_Session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# Aqui é o caminho/path do banco de dados
